56ab26899926ec6da292aa38d692c7e2e180de04
intrudershanky <intrudershanky@outlook.com>
2017-11-14 17:23:15 +0530

    Simple Game in Unity

---
+++ Assets/Script/BallControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody rb;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+		rb.AddForce (movement * speed);
+	}
+}
